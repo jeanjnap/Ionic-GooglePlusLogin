6c7241cbcf584a398e61735c1b09cb89fd366f85
JeanJNAP <jeanjnap@gmail.com>
2018-04-03 16:19:45 -0300

    implementação

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { GooglePlus } from '@ionic-native/google-plus';
+import { AngularFirestore } from 'angularfire2/firestore';
+import firebase from 'firebase'
 
 
 @Component({
